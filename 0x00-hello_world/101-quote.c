dd10ac91b0c38f6956238c876a1ca4cfeb92bebf
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-21 12:29:55 -0700

    create 0x01 variable if else while

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, 59);
 	return (1);
+
 }
