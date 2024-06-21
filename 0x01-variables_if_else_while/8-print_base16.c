ebea5823f7cca6cfa9fd9af7fae1b6743abe00ba
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-21 14:50:54 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@
 int main(void)
 {
 	char ch = '0';
+	char c = 'a';
 
 	for (ch = '0'; ch <= '9'; ch++)
 		putchar(ch);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
 
 	putchar('\n');
 	return (0);
