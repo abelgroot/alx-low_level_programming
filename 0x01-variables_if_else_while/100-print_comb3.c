3b4db4ce31112f073cfdb944e6b062aea50c165c
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-21 16:06:02 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,28 +3,28 @@
 /**
  * main - prints all possible different combinations of two digits
  *
- * Description: The program prints all unique combinations 
+ * Description: The program prints all unique combinations
  * of two different numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = i + 1; j < 10; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-            if (i < 8 || (i == 8 && j < 9))
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8 || (i == 8 && j < 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
