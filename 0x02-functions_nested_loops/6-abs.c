82c842a65e9f374c7d5b0b0fa60e19bdb1761563
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-26 12:22:00 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/* Header files go here */
+
+/**
+ * _abs - compute the absolute value of an integer
+ *@n: the number to be checked
+ *
+ * Return: absolute value of a number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+
+}
