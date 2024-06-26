2cb680beb860c3d61340b61b11194fa28bc64faa
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-26 06:50:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "_putchar.h"
+/* Header files go here */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* Header files go here */
+
+/**
+ * _putchar - Entry point
+ *
+ * Return: Always void (Success)
+ */
+void  _putchar(void)
+{
+	printf("_putchar\n");
+}
