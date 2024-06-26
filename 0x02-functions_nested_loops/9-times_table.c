0bece41c1c05637d26f09da03a143fb1ceffd105
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-26 14:07:02 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,37 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
- */
+* times_table - prints the 9 times table, starting with 0
+*/
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
 
-            // Print product with comma and spaces
-            if (col == 0)
-            {
-                _putchar('0' + product);    // Print first product without preceding comma
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (col == 0)
+			{
+				_putchar('0' + product)
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                // Print extra space for single-digit numbers
-                if (product < 10)
-                    _putchar(' ');
+				if (product < 10)
+					_putchar(' ');
 
-                // Print tens digit if product is greater than 9
-                if (product >= 10)
-                    _putchar('0' + (product / 10));
-                
-                // Print units digit
-                _putchar('0' + (product % 10));
-            }
-        }
-        _putchar('\n');    // Move to the next line after each row
-    }
+				if (product >= 10)
+					_putchar('0' + (product / 10));
+
+				_putchar('0' + (product % 10));
+			}
+		}
+		_putchar('\n');
+	}
 }
 
