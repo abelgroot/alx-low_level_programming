ce657e9a353563c6dca33e13f60f8055e05154c5
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-28 10:10:37 -0700

    4-

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,12 +14,16 @@ void print_most_numbers(void)
 
 	while (i < 10)
 	{
-		if (i == '2' || i == ''4)
+		if (i == 2 || i == 4)
 		{
+			i++;
 			continue;
 		}
-		_putchar('0' + i);
-		i++;
+		else
+		{
+			_putchar('0' + i);
+			i++;
+		}
 	}
 	_putchar('\n');
 }
