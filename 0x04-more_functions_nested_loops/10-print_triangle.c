edf4a12e519b48c7d10d0e3ecf44883fc4c4c33a
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-28 17:49:27 -0700

    10-

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_triangle - Prints a triangle followed by a new line
+* @size: The size of the triangle
+*/
+void print_triangle(int size)
+{
+	int row, col, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = size - row; space > 0; space--)
+		{
+			_putchar(' ');
+		}
+		for (col = 0; col < row; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
