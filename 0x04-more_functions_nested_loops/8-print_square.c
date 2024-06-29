9c9e013fd98cb4b9d2ab4c0d5637c87e35f895a9
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-06-28 17:16:32 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_square - Prints a square of the given size using the character #
+* @size: The size of the square
+* Return - void
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 0;
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+		_putchar('\n');
+		i++;
+		}
+	}
+}
+
