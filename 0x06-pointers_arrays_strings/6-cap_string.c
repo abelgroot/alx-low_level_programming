f830aff29e4bc22f8b3dfe688caa18b1759e0ce5
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-07-04 12:40:45 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,38 +2,30 @@
 
 /**
 * cap_string - capitalizes all words of a string
-* @str: the string to modify
+* @str: the string to be modified
 *
-* Return: a pointer to the resulting string
+* Return: a pointer to the modified string
 */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int capitalize_next = 1;
-
+	int i, j;
 	char separators[] = " \t\n,;.!?\"(){}";
 
-	while (str[i])
+	if (str[0] >= 'a' && str[0] <= 'z')
 	{
-		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
-		{
-			str[i] -= 32;
-		}
-
-		capitalize_next = 0;
-
-		for (int j = 0; separators[j] != '\0'; j++)
-		{
-			if (str[i] == separators[j])
-			{
-				capitalize_next = 1;
-				break;
-			}
-		}
-
-		i++;
+		str[0] -= 32;
 	}
 
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (str[i] == separators[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+		}
+	}
 	return (str);
 }
 
