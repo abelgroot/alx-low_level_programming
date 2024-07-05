1189f2ea43e72efc3f1e22df2603d43b81428815
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-05 18:56:23 +0300

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _strlen - function that returns the length of a string
+* _strlen - return length of a string
 * @s: an input string
 *
-* return: integer value of the string length
+* Return: integer value of the string length
 */
 
 int _strlen(char *s)
