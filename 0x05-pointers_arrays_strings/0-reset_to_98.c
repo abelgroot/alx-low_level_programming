95c06b3f1c2b439e2bbea82e5ebe68913c4db78c
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-05 18:02:19 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - takes an integer pointer change its value to 98
+* @n: an input pointer
+*
+* return: return a pointer with a value of 98
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
