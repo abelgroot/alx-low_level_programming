816c66b7d5a8245e8edb1f6488a4dfb026cbc8ec
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-05 22:55:08 +0300

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* puts_half - prints half of the string starting from n = (length_of_str - 1)/2
+* @str: input string
+*/
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	if (i % 2)
+	{
+		j = i / 2;
+	}
+	else
+		j = (i - 1) / 2;
+
+
+	for (; j < i; j++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
