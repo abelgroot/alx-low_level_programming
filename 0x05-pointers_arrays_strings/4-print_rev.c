ad5efce57f1391beaeabd0395ba543f767e6d019
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-05 19:39:50 +0300

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a newline
+* @s: input string
+*
+*/
+
+void print_rev(char *s)
+{
+	int size;
+
+	size = 0;
+
+	while (*s)
+	{
+		s++;
+		size++;
+	}
+
+	while (size >= 0)
+	{
+		_putchar(*s);
+		size--;
+		s--;
+	}
+	_putchar('\n');
+}
