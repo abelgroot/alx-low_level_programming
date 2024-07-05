bc097bd18908ecec3136715a1491faced359b5c9
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-05 23:07:30 +0300

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@ void puts_half(char *str)
 {
 	int i, j;
 
-	i = 0;
+	i = 1;
 	j = 0;
 
 	while (str[i] != '\0')
@@ -22,8 +22,8 @@ void puts_half(char *str)
 	}
 	else
 		j = (i - 1) / 2;
-
-	j--;
+	
+	j++;
 	for (; j < i; j++)
 	{
 		_putchar(str[j]);
