3c2e298791ad7cb4d59439df37f4c2353e6ca7d5
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-07-06 23:03:46 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int target_checksum = 2772;
+	char password[100];
+	int current_checksum = 0;
+	int i = 0;
+	int next_char;
+
+	srand(time(NULL));
+
+	while (current_checksum < target_checksum - 126)
+	{
+		next_char = rand() % 94 + 33; /* Generate a printable ASCII character */
+		if (current_checksum + next_char > target_checksum) 
+		{
+			next_char = target_checksum - current_checksum;
+		}
+		password[i++] = next_char;
+		current_checksum += next_char;
+	}
+
+	next_char = target_checksum - current_checksum;
+	if (next_char >= 33 && next_char <= 126)
+	{
+		password[i++] = next_char;
+	}
+	else
+	{
+		password[i++] = rand() % 94 + 33;
+	}
+	password[i] = '\0';
+
+	printf("%s", password);
+
+	return 0;
+}
+
