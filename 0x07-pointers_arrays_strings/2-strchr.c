7957ca455e65a7f01ca1d6b79d541960b11d913a
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-09 21:30:00 +0300

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: input string
+* @c: charcter to be located
+*
+* Return: the character located
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			break;
+		}
+		s++;
+	}
+	return (s);
+}
