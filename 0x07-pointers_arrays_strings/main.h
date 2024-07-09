2edf4cc3352fe90b865447e41978b6dcc123df1c
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-09 22:35:08 +0300

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ char *_strpbrk(char  *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void _putchar(char a);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
 
