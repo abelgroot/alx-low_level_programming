6bfe0576984399094470989f39add7c29509b679
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-10 22:44:21 +0300

    last

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,13 +11,13 @@ int is_palindrome(char *s)
 {
 	int len = strlen(s);
 
-    if (len <= 1) // Empty string or single character string
-        return 1;
-    
-    if (s[0] != s[len - 1])
-        return 0;
-    
-    s[len - 1] = '\0'; // Temporarily terminate string
-    return is_palindrome(s + 1);
+	if (len <= 1)
+		return (1);
+
+	if (s[0] != s[len - 1])
+		return (0);
+
+	s[len - 1] = '\0';
+	return (is_palindrome(s + 1));
 }
 
