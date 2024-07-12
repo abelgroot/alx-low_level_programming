4adb5e1076788fb993cf975d41a6c10903e2e610
abelgroot <abel.mulugeta.dagne@gmail.com>
2024-07-12 13:54:38 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of arguments passed to the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Unused parameter */
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
