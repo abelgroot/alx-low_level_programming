f74331c48fc84991d0b63f279e0fb039c48cedfb
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-16 22:13:16 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,11 +13,11 @@ int **alloc_grid(int width, int height)
 	int **grid;
 	int i, j;
 
-	if(width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	grid = malloc(height * sizeof(int *));
-	if(grid == NULL)
+	if (grid == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
