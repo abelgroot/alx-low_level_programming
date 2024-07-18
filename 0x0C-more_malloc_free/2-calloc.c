0f11198608b629be4e600c2761718e573169c48e
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-18 22:46:12 +0300

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(nmemb * size);
+	array = malloc(nmemb * size * sizeof(char));
 
 	if (array == NULL)
 		return (NULL);
