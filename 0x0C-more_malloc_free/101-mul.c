7f5b143f028e167d3f243f9d51ec6a0f92fce900
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-19 16:28:28 +0300

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -6,18 +6,18 @@
  * @c: input character
  *
  * Return: 1 if true, 0 otherwise.
-*/
+ */
 int _isdigit(char c)
 {
 	return (c >= '0' && c <= '9');
 }
 
 /**
-* is_number - checks if a string consists of only digits
-* @str: the string to check
-*
-* Return: 1 if the string is a number, 0 otherwise
-*/
+ * is_number - checks if a string consists of only digits
+ * @str: the string to check
+ *
+ * Return: 1 if the string is a number, 0 otherwise
+ */
 int is_number(char *str)
 {
 	int i = 0;
@@ -28,7 +28,7 @@ int is_number(char *str)
 		{
 			return (0);
 		}
-	i++;
+		i++;
 	}
 	return (1);
 }
@@ -41,9 +41,7 @@ int is_number(char *str)
  */
 unsigned int _strlen(char *str)
 {
-	unsigned int length;
-
-	length = 0;
+	unsigned int length = 0;
 
 	while (str[length])
 	{
@@ -51,7 +49,6 @@ unsigned int _strlen(char *str)
 	}
 	return (length);
 }
-
 
 /**
  * _puts - prints a string, followed by new line.
@@ -68,7 +65,7 @@ void _puts(char *str)
 }
 
 /**
- * _error - prints an error message and exit with status 98
+ * _error - prints an error message and exits with status 98
  */
 void _error(void)
 {
@@ -77,33 +74,30 @@ void _error(void)
 }
 
 /**
-* main - adds positive numbers passed as arguments
-* @argc: the number of command-line arguments
-* @argv: an array containing the command-line arguments
-*
-* Return: 0 if successful, 1 if an error occurs
-*/
-int main(int argc, char *argv[])
+ * initialize_result - initializes the result array with zeros
+ * @result: the result array
+ * @len_res: the length of the result array
+ */
+void initialize_result(int *result, int len_res)
 {
-	int *result;
-	unsigned int len1, len2;
-	int len_res, i, j, carry, n1, n2, sum;
+	int i;
 
-	if (argc != 3 || argv == NULL)
-		_error();
+	for (i = 0; i < len_res; i++)
+	{
+		result[i] = 0;
+	}
+}
 
-	if (!is_number(argv[1]) || !is_number(argv[2]))
-		_error();
-
-
-	len1 = _strlen(argv[1]);
-	len2 = _strlen(argv[2]);
-	len_res  = len1 + len2;
-
-	result  = malloc(len_res * sizeof(int));
-
-	if (result == NULL)
-		_error();
+/**
+ * multiply_numbers - multiplies two numbers represented as strings
+ * @argv: the array of command-line arguments
+ * @result: the result array to store the multiplication result
+ * @len1: the length of the first number string
+ * @len2: the length of the second number string
+ */
+void multiply_numbers(char *argv[], int *result, int len1, int len2)
+{
+	int i, j, carry, n1, n2, sum;
 
 	for (i = len1 - 1; i >= 0; i--)
 	{
@@ -118,20 +112,76 @@ int main(int argc, char *argv[])
 		}
 		result[i + j + 1] += carry;
 	}
+}
 
-	i = 0;
+/**
+ * print_result - prints the result array
+ * @result: the result array
+ * @len_res: the length of the result array
+ */
+void print_result(int *result, int len_res)
+{
+	int i = 0;
+
 	while (i < len_res && result[i] == 0)
+	{
 		i++;
+	}
 
 	if (i == len_res)
+	{
 		_putchar('0');
+	}
 	else
 	{
 		for (; i < len_res; i++)
+		{
 			_putchar(result[i] + '0');
+		}
 	}
 	_putchar('\n');
+}
+
+/**
+ * main - multiplies two positive numbers passed as arguments
+ * @argc: the number of command-line arguments
+ * @argv: an array containing the command-line arguments
+ *
+ * Return: 0 if successful, 1 if an error occurs
+ */
+int main(int argc, char *argv[])
+{
+	int *result;
+	unsigned int len1, len2;
+	int len_res;
+
+	if (argc != 3 || argv == NULL)
+		_error();
+
+	if (!is_number(argv[1]) || !is_number(argv[2]))
+		_error();
+
+	/* Check for multiplication by zero */
+	if (argv[1][0] == '0' || argv[2][0] == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+		return (0);
+	}
+
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
+	len_res = len1 + len2;
+
+	result = malloc(len_res * sizeof(int));
+	if (result == NULL)
+		_error();
+
+	initialize_result(result, len_res);
+	multiply_numbers(argv, result, len1, len2);
+	print_result(result, len_res);
 
 	free(result);
 	return (0);
 }
+
