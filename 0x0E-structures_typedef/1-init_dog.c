366ac30b4ca888176d993ca338c7dbc9f54be3ca
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-22 22:07:43 +0300

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,8 +7,6 @@
 * @age: the age of the dog.
 * @owner: a pointer to the name of the dog's owner.
 *
-* Description: function to initilize a variable
-* of type struct dog with the given name, age and owner.
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
