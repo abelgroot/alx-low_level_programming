4ee860d4bdfb20cddf2fc5d732bf5120d2f6add5
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-23 09:32:19 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,25 +2,41 @@
 #include "dog.h"
 
 /**
+ * _strlen - calculate the length of string.
+ * @str: input string
+ *
+ * Return: the length of string.
+ */
+
+int _strlen(char *str)
+{
+	int length;
+
+	length = 0;
+
+	while (str[length] != '\0')
+		length++;
+
+	return (length);
+}
+/**
 * new_dog - creates a new dog
 * @name: name of the dog
 * @age: age of the dog
 * @owner: owner of the dog
 *
-* Return: pointer to the new dog (dog_t), or NULL if it fails
+* Return: pointer to the new dog (dog_t),
+* or NULL if it fails
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	char *name_copy;
 	char *owner_copy;
-	int name_len = 0, owner_len = 0, i;
+	int i, name_len = 0, owner_len = 0;
 
-	while (name[name_len] != '\0')
-		name_len++;
-
-	while (owner[owner_len] != '\0')
-		owner_len++;
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
