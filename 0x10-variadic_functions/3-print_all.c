083bd7f5dd63da8a6efc801ca13175fc63d2ee5f
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-07-25 23:44:07 +0300

    al

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,45 @@
-#include <stdio.h>
 #include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_char - Prints a char.
+ * @args: The argument list.
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - Prints an integer.
+ * @args: The argument list.
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - Prints a float.
+ * @args: The argument list.
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - Prints a string.
+ * @args: The argument list.
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
 
 /**
 * print_all - Prints anything based on the format specifier.
@@ -16,38 +56,23 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0, j;
-	char *str, *separator = "";
+	char *separator = "";
+	void (*func[])(va_list) = {print_char, print_int, print_float, print_string};
+	char *formats = "cifs";
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
 		j = 0;
-
-		while ("cifs"[j])
+		while (formats[j])
 		{
-			if (format[i] == "cifs"[j])
+			if (format[i] == formats[j])
 			{
 				printf("%s", separator);
-				switch (format[i])
-				{
-					case 'c':
-						printf("%c", va_arg(args, int));
-						break;
-					case 'i':
-						printf("%d", va_arg(args, int));
-						break;
-					case 'f':
-						printf("%f", va_arg(args, double));
-						break;
-					case 's':
-						str = va_arg(args, char *);
-						if (str == NULL)
-							str = "(nil)";
-						printf("%s", str);
-						break;
-				}
+				func[j](args);
 				separator = ", ";
+			break;
 			}
 			j++;
 		}
