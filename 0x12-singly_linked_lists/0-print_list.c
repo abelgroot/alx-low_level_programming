59f19ca5d4486f722291e9142084fb95df2c6e57
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-02 22:49:19 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", ptr->len, ptr->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 		n++;
