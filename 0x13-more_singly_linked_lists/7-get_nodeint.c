9de502d32181fe1d751991ffc825c598900d2bbc
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-06 13:27:53 +0300

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	temp = head;
 
-	for (i = 0; i <= index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (temp == NULL)
 			return (NULL);
