6462459c0ee276ffb95a0f454b97b0f088d85962
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-10 10:48:26 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,18 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while ((mask << 1) <= n)
-		mask <<= 1;
-
-	while (mask)
-	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
+
