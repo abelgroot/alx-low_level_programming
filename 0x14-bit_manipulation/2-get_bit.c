f3c62f904c84e1c4c269df2494043124115ff954
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-10 13:46:10 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,6 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask = 1;
 	unsigned long int shifted = n >> index;
+
 	if (n == ULONG_MAX && index >= 64)
 		return (-1);
 	if (shifted == 0)
