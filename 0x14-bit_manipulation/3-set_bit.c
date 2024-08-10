e6c53d021b19305082002e9af1a7501d280e2704
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-08-10 15:49:38 +0300

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,8 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
 
+	if (n == ULONG_MAX && index >= 64)
+		return (-1);
 	mask = mask << index;
-
 	*n = (*n | mask);
 	return (1);
 }
