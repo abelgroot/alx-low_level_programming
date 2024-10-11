43b3cf5539cdffc94679fb07514766bd5765cab6
abel.mulugeta <abel.mulugeta.dagne@gmail.com>
2024-10-11 21:40:24 +0300

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include <strings.h>
+#include "hash_tables.h"
+
+/**
+* hash_table_get - a function that retrieves a value associated with a key.
+* @ht: is the hash table you want to look into
+* @key: is the key you are looking for
+*
+* Return: the value associated with the element, or
+* NULL if key couldn’t be found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next; /* Move to the next node in the chain */
+	}
+
+	return (NULL);
+}
